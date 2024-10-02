5a7e32f4932ed01792eb8dd638ccf6a0a3028e1e
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-10-02 12:25:18 +0530

    implement send email functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "00a7a6936d9542b5b0ad4927e6ca27ac"
 url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=00a7a6936d9542b5b0ad4927e6ca27ac"
@@ -9,7 +10,10 @@ request = requests.get(url)
 #Get data in a dictionary
 content = request.json()
 
-#Print title and description of data
+#Access title and description of data
+body = ""
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    body = body + article['title'] + "\n" + article['description'] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
