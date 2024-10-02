712c061d488436733ca645507fc4cdc40bf490c6
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-10-02 12:37:22 +0530

    improvements

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from send_email import send_email
 
 api_key = "00a7a6936d9542b5b0ad4927e6ca27ac"
-url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=00a7a6936d9542b5b0ad4927e6ca27ac"
+url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=00a7a6936d9542b5b0ad4927e6ca27ac&language=en"
 
 #Make request
 request = requests.get(url)
@@ -12,8 +12,8 @@ content = request.json()
 
 #Access title and description of data
 body = ""
-for article in content['articles']:
-    body = body + article['title'] + "\n" + article['description'] + 2*"\n"
+for article in content['articles'][:10]:
+    body = "Subject: Today's Headlines: \n" + body + article['title'] + "\n" + article['description'] + "\n" + article['url'] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
